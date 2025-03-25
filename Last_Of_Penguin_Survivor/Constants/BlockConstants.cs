428bb2120d7fc8d0b9bec1d43ec002b36eaf4438
김필원 <131760662+Develowonn@users.noreply.github.com>
2025-03-25 19:55:02 +0900

    수정 | 코드 띄어쓰기

--- Last_Of_Penguin_Survivor/Constants/BlockConstants.cs
+++ Last_Of_Penguin_Survivor/Constants/BlockConstants.cs
@@ -1,20 +1,20 @@
 public class BlockConstants
 {
-	public const string Air = nameof(Air);
-	public const string Ice = nameof(Ice);
-	public const string Water = nameof(Water);
+	public const string Air    = nameof(Air);
+	public const string Ice    = nameof(Ice);
+	public const string Water  = nameof(Water);
 	public const string Ground = nameof(Ground);
-	public const string Snow = nameof(Snow);
-	public const string Stone = nameof(Stone);
-	public const string Trees = nameof(Trees);
-	public const string Soil = nameof(Soil);
+	public const string Snow   = nameof(Snow);
+	public const string Stone  = nameof(Stone);
+	public const string Trees  = nameof(Trees);
+	public const string Soil   = nameof(Soil);
 
-	public const string TilledSoil = nameof(TilledSoil);
+	public const string TilledSoil    = nameof(TilledSoil);
 	public const string WetTilledSoil = nameof(WetTilledSoil);
 
-	public const string SilverOre = nameof(SilverOre);
-	public const string RubyOre = nameof(RubyOre);
-	public const string GoldOre = nameof(GoldOre);
+	public const string SilverOre  = nameof(SilverOre);
+	public const string RubyOre    = nameof(RubyOre);
+	public const string GoldOre    = nameof(GoldOre);
 	public const string EmelardOre = nameof(EmelardOre);
 	public const string DiamondOre = nameof(DiamondOre);
 }
